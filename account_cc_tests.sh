0993592cfef46e536c5e8a9130690f7cde377a33
abhiroop99z <99312283+abhiroop99z@users.noreply.github.com>
2024-02-26 11:04:18 +0530

    Update account_cc_tests.sh

--- account_cc_tests.sh
+++ account_cc_tests.sh
@@ -1,14 +1,68 @@
-1. Need to add couple of x100 Messages with a Systems trace audit number: For example stan2
+# 1. Need to add couple of x100 Messages with a Systems trace audit number: For example stan2
+
+export PATH=${PWD}/bin:$PATH
+export FABRIC_CFG_PATH=$PWD/../config/
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/merchantOrg1.hlfcards.com/peers/peer0.merchantOrg1.hlfcards.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/merchantOrg1.hlfcards.com/users/Admin@merchantOrg1.hlfcards.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C channel1 -n PYMTUtilsCC --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/merchantOrg1.hlfcards.com/peers/peer0.merchantOrg1.hlfcards.com/tls/ca.crt" -c '{"function":"requestTx","Args":["MID001","CID001","LR001","4","5","6","7","8","stan2","10","11","12","13","14","15","16","17","18", "19", "20", "x100"]}'
+
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C channel1 -n PYMTUtilsCC --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/merchantOrg1.hlfcards.com/peers/peer0.merchantOrg1.hlfcards.com/tls/ca.crt" -c '{"function":"requestTx","Args":["MID002","CID002","LR002","4","5","6","7","8","stan2","10","11","12","13","14","15","16","17","18", "19", "20", "x100"]}'
 
 
-2. Need to add couple of x110 messages with a systems trace audit number: For example stan2 (should be same as x100 messages)
+# 2. Need to make x100 messages to to TxConfirmed status by invoking confirm chaincode manually from AOD
 
-3. Need to make x100 messages to to TxConfirmed status by invoking confirm chaincode manually
+cd AOD
 
-4. Need to submit a x500 message from AAD to check if x100 and x110 messages are in correct status (confirmed and submitted respectively)
+export PATH=${PWD}/../bin:$PATH 
+export FABRIC_CFG_PATH=$PWD/../../config/ 
+export CORE_PEER_TLS_ENABLED=true 
+export CORE_PEER_LOCALMSPID="AODMSP" 
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/../organizations/peerOrganizations/AOD.hlfcards.com/peers/peer0.AOD.hlfcards.com/tls/ca.crt 
+export CORE_PEER_MSPCONFIGPATH=${PWD}/../organizations/peerOrganizations/AOD.hlfcards.com/users/Admin@AOD.hlfcards.com/msp 
+export CORE_PEER_ADDRESS=localhost:19051 
 
-Test:
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/../organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C channel1 -n ConfirmSettlementTxCC  --peerAddresses localhost:15051 --tlsRootCertFiles "${PWD}/../organizations/peerOrganizations/ACD.hlfcards.com/peers/peer0.ACD.hlfcards.com/tls/ca.crt" --peerAddresses localhost:11051 --tlsRootCertFiles "${PWD}/../organizations/peerOrganizations/AAD.hlfcards.com/peers/peer0.AAD.hlfcards.com/tls/ca.crt" -c '{"function":"confirmSettlementTx","Args":["x100", "MID001","CID001","LR001"]}'
 
-All x100, x110 and x500 messages with same stan number should be marked non accounted even if a single x100 message or x110 message is not confirmed or accounted respectively.
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/../organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C channel1 -n ConfirmSettlementTxCC  --peerAddresses localhost:15051 --tlsRootCertFiles "${PWD}/../organizations/peerOrganizations/ACD.hlfcards.com/peers/peer0.ACD.hlfcards.com/tls/ca.crt" --peerAddresses localhost:11051 --tlsRootCertFiles "${PWD}/../organizations/peerOrganizations/AAD.hlfcards.com/peers/peer0.AAD.hlfcards.com/tls/ca.crt" -c '{"function":"confirmSettlementTx","Args":["x100", "MID002","CID002","LR002"]}'
 
-Similarly if everything is alright, all of them should be in accounted state at the end of x500 submission.
+# 3. Need to add couple of x110 messages with a systems trace audit number: For example stan2 (should be same as x100 messages)
+
+
+cd ../ACD
+
+export PATH=${PWD}/../bin:$PATH 
+export FABRIC_CFG_PATH=$PWD/../../config/ 
+export CORE_PEER_TLS_ENABLED=true 
+export CORE_PEER_LOCALMSPID="ACDMSP" 
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/../organizations/peerOrganizations/ACD.hlfcards.com/peers/peer0.ACD.hlfcards.com/tls/ca.crt 
+export CORE_PEER_MSPCONFIGPATH=${PWD}/../organizations/peerOrganizations/ACD.hlfcards.com/users/Admin@ACD.hlfcards.com/msp 
+export CORE_PEER_ADDRESS=localhost:15051 
+
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/../organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C channel1 -n PYMTUtilsCC --peerAddresses localhost:11051 --tlsRootCertFiles "${PWD}/../organizations/peerOrganizations/AAD.hlfcards.com/peers/peer0.AAD.hlfcards.com/tls/ca.crt" --peerAddresses localhost:19051 --tlsRootCertFiles "${PWD}/../organizations/peerOrganizations/AOD.hlfcards.com/peers/peer0.AOD.hlfcards.com/tls/ca.crt" -c '{"function":"submitTx","Args":["MID001","CID001","LR001","4","5","6","7","8","stan2","10","11","12","13","14","15","16","17","18", "19", "20", "21", "22", "x110"]}'
+
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/../organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C channel1 -n PYMTUtilsCC --peerAddresses localhost:11051 --tlsRootCertFiles "${PWD}/../organizations/peerOrganizations/AAD.hlfcards.com/peers/peer0.AAD.hlfcards.com/tls/ca.crt" --peerAddresses localhost:19051 --tlsRootCertFiles "${PWD}/../organizations/peerOrganizations/AOD.hlfcards.com/peers/peer0.AOD.hlfcards.com/tls/ca.crt" -c '{"function":"submitTx","Args":["MID002","CID002","LR002","4","5","6","7","8","stan2","10","11","12","13","14","15","16","17","18", "19", "20", "21", "22", "x110"]}'
+
+
+# 4. Need to submit a x500 message from AAD to check if x100 and x110 messages are in correct status (confirmed and submitted respectively)
+cd ../AAD
+
+export PATH=${PWD}/../bin:$PATH 
+export FABRIC_CFG_PATH=$PWD/../../config/ 
+export CORE_PEER_TLS_ENABLED=true 
+export CORE_PEER_LOCALMSPID="AADMSP" 
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/../organizations/peerOrganizations/AAD.hlfcards.com/peers/peer0.AAD.hlfcards.com/tls/ca.crt 
+export CORE_PEER_MSPCONFIGPATH=${PWD}/../organizations/peerOrganizations/AAD.hlfcards.com/users/Admin@AAD.hlfcards.com/msp 
+export CORE_PEER_ADDRESS=localhost:11051 
+
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/../organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C channel1 -n PYMTUtilsCC --peerAddresses localhost:15051 --tlsRootCertFiles "${PWD}/../organizations/peerOrganizations/ACD.hlfcards.com/peers/peer0.ACD.hlfcards.com/tls/ca.crt" --peerAddresses localhost:19051 --tlsRootCertFiles "${PWD}/../organizations/peerOrganizations/AOD.hlfcards.com/peers/peer0.AOD.hlfcards.com/tls/ca.crt" -c '{"function":"accountTx","Args":["MID001","CID001","LR001","4","5","6","7","stan2","9","10","11","12","13","14","15","x500"]}'
+
+
+# Test (or what should we check for)
+
+# All x100, x110 and x500 messages with same stan number should be marked non accounted even if a single x100 message or x110 message is not confirmed or accounted respectively.
+
+# Similarly if everything is alright, all of them should be in accounted state at the end of x500 submission.
