6ae7a305ce626c44ccdaf0e158432795c102602a
abhiroop99z <99312283+abhiroop99z@users.noreply.github.com>
2024-03-06 15:45:22 +0530

    Update new invoke commands.sh

--- new invoke commands.sh
+++ new invoke commands.sh
@@ -1,6 +1,7 @@
-#Org1
 
 #Invoke x100 message request
+
+#Org1
 
 export PATH=${PWD}/bin:$PATH
 export FABRIC_CFG_PATH=$PWD/../config/
@@ -11,3 +12,25 @@ export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/merchantOr
 export CORE_PEER_ADDRESS=localhost:7051
 
 peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C channel1 -n PYMTUtilsCC --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/merchantOrg1.hlfcards.com/peers/peer0.merchantOrg1.hlfcards.com/tls/ca.crt" -c '{"function":"requestX100Tx","Args":["MID001","CID001","LR001","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18", "19", "20", "x100"]}'
+
+
+#Invoke x110 message request
+
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C channel1 -n PYMTUtilsCC -c '{"function":"requestX110Tx","Args":["MID001","CID001","LR001","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18", "19", "20", "21", "22", "x110"]}'
+
+
+#Query everything:
+
+
+#Org1
+
+export PATH=${PWD}/bin:$PATH
+export FABRIC_CFG_PATH=$PWD/../config/
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/merchantOrg1.hlfcards.com/peers/peer0.merchantOrg1.hlfcards.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/merchantOrg1.hlfcards.com/users/Admin@merchantOrg1.hlfcards.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C channel1 -n PYMTUtilsCC -c '{"function":"GetTxByRange","Args":["",""]}'
+
